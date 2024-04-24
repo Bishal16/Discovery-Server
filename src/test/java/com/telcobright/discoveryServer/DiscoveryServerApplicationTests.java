9fe9560f35667d7f9610e3d0c06006c0b82cc260
bishal16 <mahathirbishal@gmail.com>
2024-04-24 13:38:46 +0600

    First commit

---
+++ src/test/java/com/telcobright/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcobright.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
